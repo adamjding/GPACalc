ada3770558d428b4fe89d8f974b17af6ae3f89ed
Adam Ding <ding183@purdue.edu>
2019-12-06 14:20:27 -0500

    Connected to MongoDB

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const apiRouter = require("./routes/api");
 const app = express();
 
 // Connecting to Mongo database
-process.env.DATABASE_URL = 'mongodb://localhost/mydb'
+//process.env.DATABASE_URL = 'mongodb://localhost/mydb'
+console.log(process.env.DATABASE_URL)
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
